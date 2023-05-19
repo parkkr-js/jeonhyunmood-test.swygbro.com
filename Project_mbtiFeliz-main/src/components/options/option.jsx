3f638fcd4eaaa97dbc0fc91698a3c6e930df2109
ParkJiSung <gggg0195@gmail.com>
2023-05-20 05:03:18 +0900

    Committing changes to package-lock.json

--- Project_mbtiFeliz-main/src/components/options/option.jsx
+++ Project_mbtiFeliz-main/src/components/options/option.jsx
@@ -4,144 +4,124 @@ import { useHistory } from 'react-router-dom';
 import Questions from '../../common/api/questionsApi/../questionsApi';
 
 const Options = () => {
-    const [loading, setLoading] = useState(false);
-    const [num, setNum] = useState(0);
-    const [currentSlide, setCurrentSlide] = useState(1);
-    const slideRef = createRef(null);
-    const TOTAL_SLIDES = 12;
-    const history = useHistory();
-    const [mbti, setMbti] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [num, setNum] = useState(0);
+  const [currentSlide, setCurrentSlide] = useState(1);
+  const slideRef = createRef(null);
+  const TOTAL_SLIDES = 12;
+  const history = useHistory();
+  const [mbti, setMbti] = useState([]);
 
-    const nextSlideFir = () => {
-        setMbti(mbti + Questions[num].answers[0].type);
-        setNum(num + 1);
-        setCurrentSlide(currentSlide + 1);
-        slideRef.current.style.transform += 'translateX(-100vw)';
-    };
-    const nextSlideSec = () => {
-        setMbti(mbti + Questions[num].answers[1].type);
-        setNum(num + 1);
-        setCurrentSlide(currentSlide + 1);
-        slideRef.current.style.transform += 'translateX(-100vw)';
-    };
+  const nextSlideFir = () => {
+    setMbti(mbti + Questions[num].answers[0].type);
+    setNum(num + 1);
+    setCurrentSlide(currentSlide + 1);
+    slideRef.current.style.transform += 'translateX(-100vw)';
+  };
 
-    const mbtiChecker = () => {
-        setLoading(true);
-        let map = {};
-        let result = [];
-        for (let i = 0; i < mbti.length; i++) {
-            if (mbti[i] in map) {
-                map[mbti[i]] += 1;
-            } else {
-                map[mbti[i]] = 1;
-            }
-        }
-        for (let count in map) {
-            if (map[count] >= 2) {
-                result.push(count);
-            }
-        }
+  const nextSlideSec = () => {
+    setMbti(mbti + Questions[num].answers[1].type);
+    setNum(num + 1);
+    setCurrentSlide(currentSlide + 1);
+    slideRef.current.style.transform += 'translateX(-100vw)';
+  };
 
-        setTimeout(() => {
-            const examResult = result.join('');
-            history.push(`/result/${examResult}`);
-        }, 3000);
-    };
-    useEffect(() => {
-        currentSlide > TOTAL_SLIDES && mbtiChecker();
-    }, [currentSlide]);
+  const mbtiChecker = () => {
+    setLoading(true);
+    let map = {};
+    let result = [];
+    for (let i = 0; i < mbti.length; i++) {
+      if (mbti[i] in map) {
+        map[mbti[i]] += 1;
+      } else {
+        map[mbti[i]] = 1;
+      }
+    }
+    for (let count in map) {
+      if (map[count] >= 2) {
+        result.push(count);
+      }
+    }
 
-    const goBack = () => {
-        history.goBack();
-    };
+    setTimeout(() => {
+      const examResult = result.join('');
+      history.push(`/result/${examResult}`);
+    }, 3000);
+  };
 
-    return (
-        <div>
-            <section className={styles.container}>
-                <button className={styles.goback_button} onClick={goBack}>
-                    <img
-                        className={styles.back_btn}
-                        src="img/back_icon.png"
-                    />
-                    &nbsp;&nbsp;전현무 TEST
-                </button>
+  useEffect(() => {
+    currentSlide > TOTAL_SLIDES && mbtiChecker();
+  }, [currentSlide]);
 
-                {!loading && (
-                    <>
-                        <div className={styles.slider} ref={slideRef}>
-                            {Questions.map((item) => {
-                                return (
-                                    <div
-                                        className={styles.content}
-                                        key={item.id}
-                                    >
-                                        <div className={styles.top}>
-                                            <div
-                                                className={styles.mbti__counter}
-                                            >
-                                                <span
-                                                    className={
-                                                        styles.mbti__progress__color
-                                                    }
-                                                >
-                                                    {currentSlide}
-                                                </span>
-                                                <span
-                                                    className={
-                                                        styles.mbti__end__color
-                                                    }
-                                                >
-                                                    /{TOTAL_SLIDES}
-                                                </span>
-                                            </div>
-                                            <h1
-                                                className={
-                                                    styles.mbti__question
-                                                }
-                                            >
-                                                {item.question}
-                                            </h1>
-                                        </div>
-                                        <img
-                                            className={styles.logo}
-                                            src="img/angel.png"
-                                            alt="전현무"
-                                        />
-                                        <article
-                                            className={styles.mbti__btn__box}
-                                        >
-                                            <button
-                                                className={styles.mbti__button}
-                                                onClick={nextSlideFir}
-                                            >
-                                                {item.answers[0].content}
-                                            </button>
-                                            <button
-                                                className={styles.mbti__button}
-                                                onClick={nextSlideSec}
-                                            >
-                                                {item.answers[1].content}
-                                            </button>
-                                        </article>
-                                    </div>
-                                );
-                            })}
-                        </div>
-                    </>
-                )}
-                {loading && (
-                    <div className={styles.loading__container}>
-                        <img
-                            className={styles.ticket}
-                            src="img/flying.png"
-                            alt="e-ticket"
-                        />
-                        <div className={styles.loading}></div>
+  const goBack = () => {
+    history.goBack();
+  };
+
+  return (
+    <div>
+      <section className={styles.container}>
+        <button className={styles.goback_button} onClick={goBack}>
+          <img className={styles.back_btn} src="img/back_icon.png" />
+          &nbsp;&nbsp;전현무 TEST
+        </button>
+
+        {!loading && (
+          <>
+            <div className={styles.slider} ref={slideRef}>
+              {Questions.map((item) => {
+                return (
+                  <div className={styles.content} key={item.id}>
+                    <div className={styles.top}>
+                      <div className={styles.mbti__counter}>
+                        <span className={styles.mbti__progress__color}>
+                          {currentSlide}
+                        </span>
+                        <span className={styles.mbti__end__color}>
+                          /{TOTAL_SLIDES}
+                        </span>
+                      </div>
+                      <h1 className={styles.mbti__question}>
+                        {item.question}
+                      </h1>
                     </div>
-                )}
-            </section>
-        </div>
-    );
+                    <img
+                      className={styles.logo}
+                      src={item.id >= 7 ? "img/angry.png" : "img/angel.png"}
+                      alt="전현무"
+                    />
+                    <article className={styles.mbti__btn__box}>
+                      <button
+                        className={styles.mbti__button}
+                        onClick={nextSlideFir}
+                      >
+                        {item.answers[0].content}
+                      </button>
+                      <button
+                        className={styles.mbti__button}
+                        onClick={nextSlideSec}
+                      >
+                        {item.answers[1].content}
+                      </button>
+                    </article>
+                  </div>
+                );
+              })}
+            </div>
+          </>
+        )}
+        {loading && (
+          <div className={styles.loading__container}>
+            <img
+              className={styles.ticket}
+              src="img/flying.png"
+              alt="e-ticket"
+            />
+            <div className={styles.loading}></div>
+          </div>
+        )}
+      </section>
+    </div>
+  );
 };
 
 export default Options;
